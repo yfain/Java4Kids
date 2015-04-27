99e0fae603ef795ef64dc08153cbc9ba41dcaf5b
Yakov Fain <yakovfain@gmail.com>
2015-04-27 10:00:37 -0400

    modified the build.sh to us thewholebook.adoc

--- build.sh
+++ build.sh
@@ -56,7 +56,8 @@ if [ "$1" = "html" ]; then
     makeDir $OUT_DIR
 
     # list all asciidoc files in the DOCS_DIR
-    adoc_files=(./Chapter_1.adoc ./Chapter_2.adoc ./Chapter_3.adoc ./Chapter_4.adoc ./Chapter_5.adoc ./Chapter_6.adoc  ./Chapter_7.adoc ./Chapter_8.adoc ./Chapter_9.adoc ./Chapter_10.adoc ./Chapter_11.adoc ./Chapter_12.adoc ./Chapter_13.adoc ./Appendix_A.adoc)
+    # adoc_files=(./Chapter_1.adoc ./Chapter_2.adoc ./Chapter_3.adoc ./Chapter_4.adoc ./Chapter_5.adoc ./Chapter_6.adoc  ./Chapter_7.adoc ./Chapter_8.adoc ./Chapter_9.adoc ./Chapter_10.adoc ./Chapter_11.adoc ./Chapter_12.adoc ./Chapter_13.adoc ./Appendix_A.adoc)
+    adoc_files=(./thewholebook.adoc )
 
     convert `printf "%s " "${adoc_files[@]}" | cut -d " " -f 1-${#adoc_files[@]}`
 
